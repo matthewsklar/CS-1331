55e774e401b8b6418922aef4428a407607da311a
Matthew Sklar <matthewsklar227@gmail.com>
2017-09-12 12:54:54 -0400

    Main calls for necessary information

--- hw1/PgnReader.java
+++ hw1/PgnReader.java
@@ -4,45 +4,61 @@ import java.nio.file.Paths;
 public class PgnReader {
     // http://cs1331.gatech.edu/fall2017/hw1/hw1-pgn-reader.html
     public static void main(String[] args) {
-	String pgn = "NOT GIVEN";
-	
-	try {
-	    pgn = new String(Files.readAllBytes(Paths.get("fegatello.pgn")));
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
-	
-	System.out.println(tagValue("Event", pgn));
+        String pgn = "NOT GIVEN";
+
+        try {
+            pgn = new String(Files.readAllBytes(Paths.get(args[0])));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Event: " + tagValue("Event", pgn));
+        System.out.println("Site: " + tagValue("Site", pgn));
+        System.out.println("Date: " + tagValue("Date", pgn));
+        System.out.println("Round: " + tagValue("Round", pgn));
+        System.out.println("White: " + tagValue("White", pgn));
+        System.out.println("Black: " + tagValue("Black", pgn));
+        System.out.println("Result: " + tagValue("Result", pgn));
+        System.out.println("Final Position: " + finalPosition(pgn));
     }
 
-    /*
+    /**
      * Get the value corresponding to the tag name in the pgn file.
      *
-     * @param name The name of the tag
-     * @param pgn The pgn file
+     * @param name The name of the tag.
+     * @param pgn The pgn file.
      *
-     * @return A String containing the value of the tag with the given name in the pgn file.
-     * If the tag is not found, it returns the String "NOT GIVEN".
+     * @return A String containing the value of the tag with the given name
+     * in the pgn file. If the tag is not found, it returns the String
+     * "NOT GIVEN".
      */
     public static String tagValue(String name, String pgn) {
-	name = "[" + name;
-	
-	int startIndex = pgn.indexOf(name);
+        name = "[" + name;
 
-	if (startIndex == -1) {
-	    return "NOT GIVEN";
-	}
+        int startIndex = pgn.indexOf(name);
 
-	startIndex += name.length() + 2;
-	
-	int endIndex = pgn.indexOf("]", startIndex) - 1;
+        if (startIndex == -1) {
+            return "NOT GIVEN";
+        }
 
-	String value = pgn.substring(startIndex, endIndex);
+        startIndex += name.length() + 2;
 
-	return value;
+        int endIndex = pgn.indexOf("]", startIndex) - 1;
+
+        String value = pgn.substring(startIndex, endIndex);
+
+        return value;
     }
 
-    public static void finalPosition(String pgn) {
-
+    /**
+     * Get the FEN of the final position in the pgn file.
+     *
+     * @param pgn The pgn file.
+     *
+     * @return A String containing the FEN of the final position in the pgn
+     * file.
+     */
+    public static String finalPosition(String pgn) {
+        return "TODO: Complete this";
     }
 }
